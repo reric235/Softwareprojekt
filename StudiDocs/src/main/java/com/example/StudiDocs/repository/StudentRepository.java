929f3fc4c83fff23fedd3c5a6f6cc421c62ef47d
reric235 <134950465+reric235@users.noreply.github.com>
2024-11-03 14:25:26 +0100

    Delete StudiDocs directory

--- StudiDocs/src/main/java/com/example/StudiDocs/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.StudiDocs.repository;
-
-import com.example.StudiDocs.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
